6c8f05d47d908073b53d0fc2f31470a5a521df9a
jonasgaleno <jonasgaleno116@gmail.com>
2023-01-19 22:58:18 -0300

    Imagens para documentação

--- public/index.php
+++ public/index.php
@@ -9,6 +9,11 @@ require __DIR__ . '/../vendor/autoload.php';
 $caminho = $_SERVER['PATH_INFO'];
 $rotas = require __DIR__ . '../../config/routes.php';
 
+if (empty($caminho)) {
+    header('Location: /login');
+    exit();
+}
+
 if (!array_key_exists($caminho, $rotas)) {
     echo 'error 404';
     http_response_code(404);
